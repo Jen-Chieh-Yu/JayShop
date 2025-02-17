21a009e49aefdabc5b7d0fc28b1cfac5912926c6
Jen-Chieh-Yu <xlarge840516@gmail.com>
2025-02-17 18:48:17 +0800

    新增Cart頁面的ViewModel。

---
+++ ViewModel/CartOrderViewModel.cs
@@ -0,0 +1,10 @@
+﻿using JayShop.Models;
+
+namespace JayShop.ViewModel
+{
+    public class CartViewModel
+    {
+        public Cart? Cart { get; set; }
+        public Order? Order { get; set; }
+    }
+}
