67adac3e6ba8770268a78a6c0792119ea604437b
Jen-Chieh-Yu <xlarge840516@gmail.com>
2025-02-17 18:41:03 +0800

    。

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,6 +24,11 @@ namespace JayShop.Controllers
             return View();
         }
 
+        public IActionResult T()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
