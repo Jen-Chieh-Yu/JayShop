b1f602f2a6d89237bccbfb690eee3b96354c2309
Jen-Chieh-Yu <xlarge840516@gmail.com>
2025-02-17 18:41:32 +0800

    刪除不必要的Action。

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,11 +24,6 @@ namespace JayShop.Controllers
             return View();
         }
 
-        public IActionResult T()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
